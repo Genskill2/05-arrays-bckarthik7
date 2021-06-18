8b1efde521d907e8b50bf3965cf794990d6ab8f8
bckarthik7 <44445298+bckarthik7@users.noreply.github.com>
2021-06-19 02:29:56 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 #include <limits.h>
 
-float average(int x[], int n){
 
+int prime(int);
+
+
+float average(int x[], int n){
 float s = 0.0,avg;
 for(int i=0;i<n;i++)
   s=s+x[i];
   avg=s/n;
 return avg;
 }
+
 
 int max(int x[], int n){
   int m=INT_MIN;
@@ -18,6 +22,8 @@ int max(int x[], int n){
       m=x[i];
   return m;
 }
+
+
 int min(int x[], int n){
   int m=INT_MAX;
   for(int i=0;i<n;i++)
@@ -25,6 +31,7 @@ int min(int x[], int n){
       m=x[i];
   return m;
 }
+
 
 int mode(int x[], int n){
 
@@ -44,4 +51,29 @@ int mode(int x[], int n){
       k++;
     }
   return x[k-1];
+}
+
+
+int factors(int n, int a[]){
+  int j=0,k=0,m=n;
+  for(int i=2;i<=m/2;i++){
+    if(prime(i)==1)
+      while(n%i==0){
+        a[j]=i;
+        n=n/i;
+        j++;
+      }
+  }
+  return j;
+  for(int i=0;i<j;i++)
+}
+
+int prime(int i){
+  int c=0;
+  for(int j=1;j<=i;j++)
+    if(i%j==0)
+      c++;
+  if(c==2)
+  return 1;
+  else return 0;
 }
